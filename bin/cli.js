3464b4df237aee65ca169a67fac839019e6fd71e
Shakeel Ahmed <shakeel.ahmed@digital.datamatics.com>
2024-11-18 02:25:38 +0530

    Add colors to cli.js console

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { execSync } from "child_process";
+import chalk from "chalk";
 
 const runCommand = command => {
   try {
@@ -15,14 +16,17 @@ const runCommand = command => {
 const repoName = process.argv[2];
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/ahmedshakeel083/react-starter-kit.git ${repoName}`;
 const installDepsCommand = `cd ${repoName} && npm install`;
+const runLocalHostCommand = `cd ${repoName} && npm run dev`;
 
-console.log(`Cloning the repositry with name ${repoName}`);
+console.log(chalk.green(`Cloning the repositry with name ${repoName}`));
 const checkedOut = runCommand(gitCheckoutCommand);
 if(!checkedOut) process.exit(1);
 
-console.log(`Installing dependecies for ${repoName}`);
+console.log("\x1b[32m%s\x1b[0m", `Installing dependecies for ${repoName}`);
 const installedDeps = runCommand(installDepsCommand);
 if (!installedDeps) process.exit(1);
 
-console.log("Congratulations, You can start the development now");
-console.log(`cd ${repoName} && npm run dev`);
+console.log("\x1b[32m%s\x1b[0m", "Congratulations, You can start the development now");
+
+const runLocalHost = runCommand(runLocalHostCommand);
+if(!runLocalHost) process.exit(1);
